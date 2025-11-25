1c61fe396ee66fb18ceb931acd2924a9be5400f2
Jon Daniel <joneqdaniel@gmail.com>
2025-11-25 23:10:27 +0000

    update vector example

--- examples/vec.cpp
+++ examples/vec.cpp
@@ -5,21 +5,29 @@
  */
 #include "vec.hpp"
 
-using f32    = float;
-using f32x2  = vec<f32,2>;
+using f32   = float;
+using f32x2 = vec<f32,2>;
+
+bool print_test(f32x2 a, f32x2 b);
 
 int main(int argc, char** argv)
 {
-    f32x2 a = (f32x2){ cosf(  M_PIf/6), sinf(  M_PIf/6) } * 3.0f;
-    f32x2 b = (f32x2){ cosf(2*M_PIf/3), sinf(2*M_PIf/3) } * 5.0f;
+    f32x2  a = f32x2{ cosf(  M_PIf/6), sinf(  M_PIf/6) } * 3;
+    f32x2  b = f32x2{ cosf(2*M_PIf/3), sinf(2*M_PIf/3) } * 5;
 
-    a.print(); fputs(" + ", stdout); b.print();                    fputs(" = ", stdout);
+    exit(print_test(a,b) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
+bool print_test(f32x2 a, f32x2 b)
+{
+    a.print(); fputs(" + ", stdout); b.print(); fputs(" = ", stdout);
     (a + b).println();
-    a.print(); fputs(" - ", stdout); b.print();                    fputs(" = ", stdout);
+    a.print(); fputs(" - ", stdout); b.print(); fputs(" = ", stdout);
     (a - b).println();
-    a.print(); fputs(" * 3                             ", stdout); fputs(" = ", stdout);
+
+    a.print(); fputs(" * 3                              = ", stdout);
     (a * 3.0f).println();
-    a.print(); fputs(" / 2.5                           ", stdout); fputs(" = ", stdout);
+    b.print(); fputs(" / 2.5                            = ", stdout);
     (b / 2.5f).println();
-    exit(EXIT_SUCCESS);
+    return true;
 }
