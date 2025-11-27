e0cd6c847452169ccd79567128659cc994a23d8b
Jon Daniel <joneqdaniel@gmail.com>
2025-11-27 19:03:45 +0000

    sync det example

--- examples/det.cpp
+++ examples/det.cpp
@@ -1,14 +1,7 @@
 #include "mat.hpp"
 
 template<typename T, size_t N, size_t M = N, enum alg A = alg::std>
-constexpr inline void test(mat<T,N,M,A>& src)
-{
-	src.println();
-	printf("  permanent: %+e\n",               (flt)src.perm());
-	printf("determinant: %+e\n",                (flt)src.det());
-	printf(" align/size: %zu/%zu\n", alignof(src), sizeof(src));
-	puts("");
-}
+constexpr inline bool test(mat<T,N,M,A>& src);
 
 int main()
 {
@@ -39,20 +32,16 @@ int main()
 		{20, 21, 22, 23, 24}
 	};
 
-	test(A);
-	test(B);
-	test(C);
-	test(D);
+	exit(test(A) && test(B) && test(C) && test(D) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
 
-	mat<float,2> E = { vec<float,2>{ cosf(  M_PIf/6), sinf(  M_PIf/6) } * 3,
-	                   vec<float,2>{ cosf(2*M_PIf/3), sinf(2*M_PIf/3) } * 5  };
-
-	(E[0]       ).println();
-	(E[1]       ).println();
-	(E[0] + E[1]).println();
-	(E[0] - E[1]).println();
-	(E[0] * 3   ).println();
-	(E[1] / 2.5 ).println();
-
-	return EXIT_SUCCESS;
+template<typename T, size_t N, size_t M, enum alg A>
+constexpr inline bool test(mat<T,N,M,A>& src)
+{
+	src.println();
+	printf("  permanent: %+e\n",               (flt)src.perm());
+	printf("determinant: %+e\n",                (flt)src.det());
+	printf(" align/size: %zu/%zu\n", alignof(src), sizeof(src));
+	puts("");
+	return true;
 }
